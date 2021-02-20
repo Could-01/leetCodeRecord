2897c958afc538b88b20f5deb25bb7e668f8ab5e
Could-01 <892146380@qq.com>
2021-02-20 16:20:39 +0800

    leetcode codes for Java

--- 数组/_27_移除元素.java
+++ 数组/_27_移除元素.java
@@ -14,15 +14,30 @@ public class _27_移除元素 {
                 i--;
             }
         }
-
         return l;
+    }
 
+    public int removeElement1(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int left = 0, right = nums.length - 1, count = 0;
+        while (left <= right) {
+            while (nums[right] == val && right > left) right--;
+            if (nums[left] == val) {
+                nums[left] = nums[right];
+                right--;
+                left--;
+            } else {
+                count++;
+            }
+            left++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         _27_移除元素 a = new _27_移除元素();
         int[] data = {0, 1, 2, 2, 3, 0, 4, 2};
-        System.out.println(a.removeElement(data, 2));
+        System.out.println(a.removeElement1(data, 2));
         System.out.println(Arrays.toString(data));
     }
 }

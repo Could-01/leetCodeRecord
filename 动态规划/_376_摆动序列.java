e59a1e0a52f6f65a2643c0048c1a83702e6c117f
Could-01 <892146380@qq.com>
2021-02-23 20:57:10 +0800

    leetcode codes for Java

--- 动态规划/_376_摆动序列.java
+++ 动态规划/_376_摆动序列.java
@@ -14,6 +14,7 @@ public class _376_摆动序列 {
             dp1[i] = dp1[i - 1];
             dp2[i] = dp2[i - 1];
             // 只有其中一个更新了，另一个才会更近，相互制约
+            // 大神的思路太强了！
             if (nums[i] > nums[i - 1]) {
                 dp1[i] = dp2[i] + 1;
             } else if (nums[i] < nums[i - 1]) {
@@ -22,6 +23,28 @@ public class _376_摆动序列 {
         }
         return Math.max(dp1[n - 1], dp2[n - 1]);
     }
+
+    // 换一个思路看问题
+    // 相互制约
+    public int wiggleMaxLength1(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int up = 1;
+        int down = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            }
+            if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+
+
 
     public static void main(String[] args) {
         _376_摆动序列 a = new _376_摆动序列();

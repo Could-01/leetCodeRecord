f5f6368239ec31594c2a7e4857819abe96cd15b9
Could-01 <892146380@qq.com>
2021-02-26 00:02:58 +0800

    leetcode codes for Java

--- 动态规划/_84_柱状图中最大的矩形.java
+++ 动态规划/_84_柱状图中最大的矩形.java
@@ -33,9 +33,29 @@ public class _84_柱状图中最大的矩形 {
         return res;
     }
 
+
+    public int largestRectangleArea1(int[] heights) {
+        int length = heights.length, area = 0;
+        int[] tmp = new int[length + 2];
+        System.arraycopy(heights, 0, tmp, 1, length);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < tmp.length; i++) {
+            // 对栈中柱体来说，栈中的下一个柱体就是其「左边第一个小于自身的柱体」；
+            // 若当前柱体 i 的高度小于栈顶柱体的高度，说明 i 是栈顶柱体的「右边第一个小于栈顶柱体的柱体」。
+            // 因此以栈顶柱体为高的矩形的左右宽度边界就确定了，可以计算面积～
+            while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
+                int h = tmp[stack.pop()];
+                area = Math.max(area, (i - stack.peek() - 1) * h);
+            }
+            stack.push(i);
+        }
+        return area;
+    }
+
     public static void main(String[] args) {
         _84_柱状图中最大的矩形 a = new _84_柱状图中最大的矩形();
         int[] data = {2, 1, 5, 6, 2, 3};
-        System.out.println(a.largestRectangleArea(data));
+        System.out.println(a.largestRectangleArea1(data));
     }
 }

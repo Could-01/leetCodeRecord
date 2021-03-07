499a77a64167629b7a16ed7fb2dd17ac50929ef0
Could-01 <892146380@qq.com>
2021-03-07 18:18:24 +0800

    leetcode codes for Java

---
+++ 数组/_5698_构成特定和需要添加的最少元素.java
@@ -0,0 +1,19 @@
+package 数组;
+
+public class _5698_构成特定和需要添加的最少元素 {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int item : nums) {
+            sum += item;
+        }
+        long diff = sum - goal;
+        return diff % limit == 0 ? (int) Math.abs(diff / limit) : (int) Math.abs(diff / limit) + 1;
+    }
+
+    public static void main(String[] args) {
+        _5698_构成特定和需要添加的最少元素 a = new _5698_构成特定和需要添加的最少元素();
+        int[] data = {1, -1, 1};
+        System.out.println(a.minElements(data, 3, -4));
+    }
+
+}

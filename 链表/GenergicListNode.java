ba80b62f72ef1cb4c49a022361e5511ddca2ca9d
Could-01 <892146380@qq.com>
2021-03-11 14:36:13 +0800

    leetcode codes for Java

--- 链表/GenergicListNode.java
+++ 链表/GenergicListNode.java
@@ -1,17 +1,33 @@
 package 链表;
 
+
 public class GenergicListNode {
     ListNode getListNode(int[] list) {
-        if (list.length == 0) return new ListNode();
-        ListNode res = new ListNode(list[0]);
-        ListNode tmp = new ListNode();
+        ListNode res = new ListNode(0);
+        ListNode tmp = new ListNode(0);
         res.next = tmp;
-        for (int i = 1; i < list.length; i++) {
-            tmp.next = new ListNode(list[i]);
+        for (int i = 0; i < list.length; i++) {
+            tmp.val = list[i];
+            tmp.next = new ListNode(0);
+            if (i == list.length - 1) {
+                tmp.next = null;
+                return res.next;
+            }
             tmp = tmp.next;
         }
-        ListNode l = res.next.next;
-        res.next = l;
-        return res;
+        return null;
+    }
+
+    String GetListNodeString(int[] list) {
+        ListNode node = getListNode(list);
+        StringBuilder sb = new StringBuilder(node.toString());
+        int length = sb.length();
+        sb.delete(length - 8, length);
+        return sb.toString();
+    }
+
+
+    void PrintListNodeString(int[] list) {
+        System.out.println(GetListNodeString(list));
     }
 }
--- 链表/ListNode.java
+++ 链表/ListNode.java
@@ -1,14 +1,32 @@
 package 链表;
 
 public class ListNode {
-    int val;
+    Integer val;
     ListNode next;
+
+    boolean flag = true; // 用来标记是否到了最后一个
 
     ListNode() {
     }
 
     ListNode(int val) {
         this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode getNext() {
+        return next;
     }
 
     ListNode(int val, ListNode next) {
@@ -18,11 +36,18 @@ public class ListNode {
 
     @Override
     public String toString() {
-//        return "ListNode{" +
-//                "val=" + val +
-//                ", next=" + next +
-//                '}';
-        return val + "->" + next + "->";
+        if ((next == null || val == null) && !flag) {
+            return null;
+        }
 
+        if (next != null && val != null) {
+            return val + "->" + next + "->";
+        } else if (flag) {
+            flag = false;
+            return val + "";
+        }
+
+
+        return null;
     }
 }

0eef6f63c4246af64a1a46dd92d61ee06b02ede5
Could-01 <892146380@qq.com>
2021-03-18 08:49:09 +0800

    leetcode codes for Java

--- 数组/_54_螺旋矩阵.java
+++ 数组/_54_螺旋矩阵.java
@@ -63,10 +63,61 @@ public class _54_螺旋矩阵 {
         return result;
     }
 
+    public List<Integer> spiralOrder1(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int ml = matrix.length, m0l = matrix[0].length;
+        if (ml == 0 || m0l == 0) return res;
+        int[] position = {0, 1, 0, -1, 0};
+        int row = 0, col = 0;
+        int index = 0;
+        // row行 col列
+        while (res.size() != ml * m0l) {
+            int num = matrix[row][col];
+            if (num != 101) res.add(num);
+            matrix[row][col] = 101;
+            row += position[index];
+            col += position[index + 1];
+            if (col == m0l || row == ml || col == -1 || row == -1 || matrix[row][col] == 101) {
+                index++;
+                if (row == ml) {
+                    row--;
+                    col += position[index + 1];
+                    continue;
+                }
+
+                if (col == m0l) {
+                    col--;
+                    row += position[index];
+                    continue;
+                }
+
+                if (col == -1) {
+                    row--;
+                    col++;
+                }
+
+                if (matrix[row][col] == 101) {
+                    index--;
+                    row -= position[index];
+                    col -= position[index + 1];
+                    index++;
+                }
+
+                if (index == 4) {
+                    index = 0;
+                }
+            }
+
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         _54_螺旋矩阵 a = new _54_螺旋矩阵();
+        int[][] data1 = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
         int[][] data = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
-        System.out.println(a.spiralOrder(data));
+        System.out.println(a.spiralOrder1(data));
 
     }
 }

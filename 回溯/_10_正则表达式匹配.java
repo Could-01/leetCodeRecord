68e2c437352ae557888b77d3932bea3de1c23713
Could-01 <892146380@qq.com>
2021-04-04 18:42:11 +0800

    leetcode codes for Java

---
+++ 回溯/_10_正则表达式匹配.java
@@ -0,0 +1,28 @@
+package 回溯;
+
+public class _10_正则表达式匹配 {
+    public boolean isMatch(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+        boolean[][] memory = new boolean[sLen + 1][pLen + 1];
+        memory[0][0] = true;
+        for (int i = 0; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    memory[i][j] = memory[i][j - 2] ||
+                            (i > 0 && (s.charAt(i - 1) == p.charAt(j - 2) ||
+                                    p.charAt(j - 2) == '.') && memory[i - 1][j]);
+                } else {
+                    memory[i][j] = i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) ||
+                            p.charAt(j - 1) == '.') &&
+                            memory[i - 1][j - 1];
+                }
+            }
+        }
+        return memory[sLen][pLen];
+    }
+
+    public static void main(String[] args) {
+        _10_正则表达式匹配 a = new _10_正则表达式匹配();
+        System.out.println(a.isMatch("mississippi", "mis*is*ip*."));
+    }
+}

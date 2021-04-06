cd9a7bc6aa7f949b7dceaada73de73abeb03b0f1
Could-01 <892146380@qq.com>
2021-04-06 21:34:31 +0800

    leetcode codes for Java

---
+++ 数组/_127_单词接龙.java
@@ -0,0 +1,323 @@
+package 数组;
+
+import java.util.*;
+
+public class _127_单词接龙 {
+
+    // 做不出来的
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        _127_单词接龙 a = new _127_单词接龙();
+        String[] data = {"hot", "dot", "dog", "lot", "log", "cog"};
+        List<String> list = Arrays.asList(data);
+        System.out.println(a.ladderLength("hit", "cog", list));
+    }
+}
+
+class Solution1 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        visited.add(beginWord);
+        int count = 0;
+        while (queue.size() > 0) {
+            int size = queue.size();
+            ++count;
+            for (int i = 0; i < size; ++i) {
+                String start = queue.poll();
+                for (String s : wordList) {
+                    // 已经遍历的不再重复遍历
+                    if (visited.contains(s)) {
+                        continue;
+                    }
+                    // 不能转换的直接跳过
+                    if (!canConvert(start, s)) {
+                        continue;
+                    }
+                    // 用于调试
+                    // System.out.println(count + ": " + start + "->" + s);
+                    // 可以转换，并且能转换成 endWord，则返回 count
+                    if (s.equals(endWord)) {
+                        return count + 1;
+                    }
+                    // 保存访问过的单词，同时把单词放进队列，用于下一层的访问
+                    visited.add(s);
+                    queue.offer(s);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean canConvert(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int count = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++count;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+}
+
+class Solution2 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        // visited 修改为 boolean 数组
+        boolean[] visited = new boolean[wordList.size()];
+        int idx = wordList.indexOf(beginWord);
+        if (idx != -1) {
+            visited[idx] = true;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int count = 0;
+        while (queue.size() > 0) {
+            int size = queue.size();
+            ++count;
+            while (size-- > 0) {
+                String start = queue.poll();
+                for (int i = 0; i < wordList.size(); ++i) {
+                    // 通过 index 判断是否已经访问
+                    if (visited[i]) {
+                        continue;
+                    }
+                    String s = wordList.get(i);
+                    if (!canConvert(start, s)) {
+                        continue;
+                    }
+                    if (s.equals(endWord)) {
+                        return count + 1;
+                    }
+                    visited[i] = true;
+                    queue.offer(s);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean canConvert(String s1, String s2) {
+        // 因为题目说了单词长度相同，可以不考虑长度问题
+        // if (s1.length() != s2.length()) return false;
+        int count = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++count;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+}
+
+class Solution3 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+        int start = wordList.size() - 1;
+        // 用于 BFS 遍历的队列
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
+        // 用于保存已访问的单词
+        Set<Integer> visited1 = new HashSet<>();
+        Set<Integer> visited2 = new HashSet<>();
+        queue1.offer(start);
+        queue2.offer(end);
+        visited1.add(start);
+        visited2.add(end);
+        int count1 = 0;
+        int count2 = 0;
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count1++;
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = wordList.get(queue1.poll());
+                for (int i = 0; i < wordList.size(); ++i) {
+                    if (visited1.contains(i)) {
+                        continue;
+                    }
+                    if (!canConvert(s, wordList.get(i))) {
+                        continue;
+                    }
+                    if (visited2.contains(i)) {
+                        return count1 + count2 + 1;
+                    }
+                    visited1.add(i);
+                    queue1.offer(i);
+                }
+            }
+            count2++;
+            int size2 = queue2.size();
+            while (size2-- > 0) {
+                String s = wordList.get(queue2.poll());
+                for (int i = 0; i < wordList.size(); ++i) {
+                    if (visited2.contains(i)) {
+                        continue;
+                    }
+                    if (!canConvert(s, wordList.get(i))) {
+                        continue;
+                    }
+                    if (visited1.contains(i)) {
+                        return count1 + count2 + 1;
+                    }
+                    visited2.add(i);
+                    queue2.offer(i);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean canConvert(String a, String b) {
+        int count = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a.charAt(i) != b.charAt(i)) {
+                if (++count > 1) return false;
+            }
+        }
+        return count == 1;
+    }
+}
+
+class Solution4 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+        int start = wordList.size() - 1;
+        Queue<Integer> queue1 = new LinkedList<>();
+        Queue<Integer> queue2 = new LinkedList<>();
+        Set<Integer> visited1 = new HashSet<>();
+        Set<Integer> visited2 = new HashSet<>();
+        queue1.offer(start);
+        queue2.offer(end);
+        visited1.add(start);
+        visited2.add(end);
+        int count = 0;
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count++;
+            if (queue1.size() > queue2.size()) {
+                Queue<Integer> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<Integer> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = wordList.get(queue1.poll());
+                for (int i = 0; i < wordList.size(); ++i) {
+                    if (visited1.contains(i)) {
+                        continue;
+                    }
+                    if (!canConvert(s, wordList.get(i))) {
+                        continue;
+                    }
+                    if (visited2.contains(i)) {
+                        return count + 1;
+                    }
+                    visited1.add(i);
+                    queue1.offer(i);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public boolean canConvert(String a, String b) {
+        int count = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a.charAt(i) != b.charAt(i)) {
+                if (++count > 1) return false;
+            }
+        }
+        return count == 1;
+    }
+}
+
+class Solution5 {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int end = wordList.indexOf(endWord);
+        if (end == -1) {
+            return 0;
+        }
+        wordList.add(beginWord);
+
+        // 从两端 BFS 遍历要用的队列
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        // 两端已经遍历过的节点
+        Set<String> visited1 = new HashSet<>();
+        Set<String> visited2 = new HashSet<>();
+        queue1.offer(beginWord);
+        queue2.offer(endWord);
+        visited1.add(beginWord);
+        visited2.add(endWord);
+
+        int count = 0;
+        Set<String> allWordSet = new HashSet<>(wordList);
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count++;
+            if (queue1.size() > queue2.size()) {
+                Queue<String> tmp = queue1;
+                queue1 = queue2;
+                queue2 = tmp;
+                Set<String> t = visited1;
+                visited1 = visited2;
+                visited2 = t;
+            }
+            int size1 = queue1.size();
+            while (size1-- > 0) {
+                String s = queue1.poll();
+                char[] chars = s.toCharArray();
+                for (int j = 0; j < s.length(); ++j) {
+                    // 保存第j位的原始字符
+                    char c0 = chars[j];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        chars[j] = c;
+                        String newString = new String(chars);
+                        // 已经访问过了，跳过
+                        if (visited1.contains(newString)) {
+                            continue;
+                        }
+                        // 两端遍历相遇，结束遍历，返回 count
+                        if (visited2.contains(newString)) {
+                            return count + 1;
+                        }
+                        // 如果单词在列表中存在，将其添加到队列，并标记为已访问
+                        if (allWordSet.contains(newString)) {
+                            queue1.offer(newString);
+                            visited1.add(newString);
+                        }
+                    }
+                    // 恢复第j位的原始字符
+                    chars[j] = c0;
+                }
+            }
+        }
+        return 0;
+    }
+}

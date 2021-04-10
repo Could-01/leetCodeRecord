911052e4a5ec232ca1c259f646de7a3092f7b51e
Could-01 <892146380@qq.com>
2021-04-10 13:32:29 +0800

    leetcode codes for Java

---
+++ 数组/_217_存在重复元素.java
@@ -0,0 +1,15 @@
+package 数组;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _217_存在重复元素 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i : nums) {
+            if (set.contains(i)) return true;
+            set.add(i);
+        }
+        return false;
+    }
+}

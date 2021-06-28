56e1680421b87068540041fee08de50fbd4a40e6
Could-01 <892146380@qq.com>
2021-06-28 14:04:54 +0800

    leetcode codes for Java

---
+++ 数组/_240_搜索二维矩阵II.java
@@ -0,0 +1,25 @@
+package 数组;
+
+public class _240_搜索二维矩阵II {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) return false;
+        int m = 0;
+        int n = matrix[0].length - 1;
+        while (m < matrix.length && n >= 0) {
+            if (matrix[m][n] == target) {
+                return true;
+            } else if (matrix[m][n] > target) {
+                n--;
+            } else {
+                m++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        _240_搜索二维矩阵II a = new _240_搜索二维矩阵II();
+        int[][] data = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        System.out.println(a.searchMatrix(data, 5));
+    }
+}
